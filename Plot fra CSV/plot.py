f412c3f2b76335095e460a9a21f1bf21a79057d3
njaalsb <bruheimnjal@gmail.com>
2025-05-08 11:23:01 +0200

    CSV og FFT

--- Plot fra CSV/plot.py
+++ Plot fra CSV/plot.py
@@ -8,7 +8,7 @@ lst1 = []
 
 columns = ['Time (s)', 'Channel 1 (V)', 'Channel 2 (V)']
 
-df = pd.read_csv('enhetspulsrespons0.42.csv', usecols= columns)
+df = pd.read_csv(r'CSV filer\Hele_programmet.csv', usecols= columns)
 
 x_values = df['Time (s)']
 
@@ -23,9 +23,9 @@ for i in range (8193):
     lst1.append(1)
 
 #plot
-plt.title("Enhetspulsrespons")
+plt.title("Tidtaking-Programmet")
 #plt.plot(x_values, y_values, x_values, lst, x_values, lst1)
-plt.plot(x_values, input_values, label="y [n]")
+# plt.plot(x_values, input_values, label="y [n]")
 plt.plot(x_values, y_values, label="Utgangssignal")
 # plt.plot(2062.5, -4.1519, 'bo', label='f_c (2062.5, -4.1519)')
 # plt.plot(2750, -10.1092, 'ro', label='f_n (2750, -10.1092)')
@@ -36,6 +36,8 @@ plt.xlabel("T[s]")
 plt.ylabel("Amplitude [V]")
 plt.grid(True)
 #plt.plot(x_values, y_values)
+plt.axvline(x=-1.160590530611085, color = 'b', label = 'start, x = -1.160s')
+plt.axvline(x=-0.8683114606110847, color = 'b', label = 'start, x = -0.868s')
 plt.legend()
 plt.show()
 
